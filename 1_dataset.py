12da7f1bb2644d6446925595ad4b2f0821225fe8
jeongjoonsup <penny4860@gmail.com>
2017-09-30 13:16:05 +0900

    unused comment deleted

--- 1_dataset.py
+++ 1_dataset.py
@@ -20,16 +20,3 @@ if __name__ == "__main__":
     FileHDF5.write(pos_imgs, os.path.join(os.path.dirname(__file__), "car_db.hdf5"), "pos_imgs", "w")
     FileHDF5.write(neg_imgs, os.path.join(os.path.dirname(__file__), "car_db.hdf5"), "neg_imgs", "a")
     
-#     img = pos_imgs[0]
-#     from skimage.feature import hog
-#     orient = 9
-#     pix_per_cell = 8
-#     cell_per_block = 2
-#     feature_array = hog(img,
-#                         orientations=orient,
-#                         pixels_per_cell=(pix_per_cell, pix_per_cell),
-#                         cells_per_block=(cell_per_block, cell_per_block),
-#                         visualise=False,
-#                         feature_vector=True)
-#     print(feature_array.shape) # (7, 7, 2, 2, 9)
-
