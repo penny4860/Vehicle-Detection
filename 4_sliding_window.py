f051425bc5ccd4c2550baa392446d1a286dddd94
jeongjoonsup <penny4860@gmail.com>
2017-09-30 21:39:44 +0900

    ImgScanner class refactoring

--- 4_sliding_window.py
+++ 4_sliding_window.py
@@ -2,15 +2,19 @@
 
 import cv2
 
-
 class ImgScanner(object):
     
-    def __init__(self, image, step_y=10, step_x=10, win_y=30, win_x=30):
+    def __init__(self, image, step_y=5, step_x=5, win_y=64, win_x=64):
         self._layer = image
         self._step_x = step_x
         self._step_y = step_y
         self._win_x = win_x
         self._win_y = win_y
+        
+        self.x1 = None
+        self.x2 = None
+        self.y1 = None
+        self.y2 = None
         
     def generate_next(self):
         """Generate next patch
@@ -23,16 +27,27 @@ class ImgScanner(object):
         
         for y in range(0, self._layer.shape[0] - self._win_y, self._step_y):
             for x in range(0, self._layer.shape[1] - self._win_x, self._step_x):
-                yield (y, x, self._layer[y:y + self._win_y, x:x + self._win_x])
+                self.y1 = y
+                self.y2 = y + self._win_y
+                self.x1 = x
+                self.x2 = x + self._win_x
+                patch = self._layer[self.y1:self.y2, self.x1:self.x2]
+                yield patch
 
+    def get_bb(self):
+        p1 = (self.x1, self.y1)
+        p2 = (self.x2, self.y2)
+        return p1, p2
+        
     def get_patches(self):
-        patches = [window for _, _, window in image_scanner.generate_next()]
+        patches = [patch for patch in self.generate_next()]
         return patches
 
     def show_process(self):
-        for y, x, _ in self.generate_next():
+        for _ in self.generate_next():
             clone = self._layer.copy()
-            cv2.rectangle(clone, (x, y), (x + self._win_x, y + self._win_y), (0, 255, 0), 2)
+            p1, p2 = self.get_bb()
+            cv2.rectangle(clone, p1, p2, (0, 255, 0), 2)
             cv2.imshow("Test Image Scanner", clone)
             cv2.waitKey(1)
             time.sleep(0.025)
@@ -40,38 +55,64 @@ class ImgScanner(object):
 
 class ImgPyramid(object):
      
-    def __init__(self, image, scale=0.7, min_y=30, min_x=30):
-        self._layer = image.copy()
-        self._scale_for_original = 1.0
+    def __init__(self, image, scale=0.7, min_y=64, min_x=64):
+        self.layer = image.copy()
+        self.scale_for_original = 1.0
         
         self._scale = scale
         self._min_y = min_y
         self._min_x = min_x
      
     def generate_next(self):
-        yield self._layer, self._scale_for_original
+        yield self.layer, self.scale_for_original
  
         while True:
-            h = int(self._layer.shape[0] * self._scale)
-            w = int(self._layer.shape[1] * self._scale)
+            h = int(self.layer.shape[0] * self._scale)
+            w = int(self.layer.shape[1] * self._scale)
              
-            self._layer = cv2.resize(self._layer, (w, h))
+            self.layer = cv2.resize(self.layer, (w, h))
              
             if h < self._min_y or w < self._min_x:
                 break
-            self._scale_for_original = self._scale_for_original * self._scale
-            yield self._layer, self._scale_for_original
+            self.scale_for_original = self.scale_for_original * self._scale
+            yield self.layer
+
+
+class Scanner(object):
+    
+    def __init__(self, image):
+        self._image = image
+        self.layer = None
+    
+    def generate_next(self):
+        image_pyramid = ImgPyramid(self._image)
+        for layer in image_pyramid.generate_next():
+            scanner = ImgScanner(layer)
+            self.layer = layer
+            for patch, y1, y2, x1, x2 in scanner.generate_next():
+                y1_, y2_, x1_, x2_ = self._get_bb(y1, y2, x1, x2, image_pyramid.scale_for_original)
+                yield patch, y1_, y2_, x1_, x2_
+    
+    def show_process(self):
+        for patch, _, _, _, _ in self.generate_next():
+            clone = self.layer.copy()
+            cv2.rectangle(clone, (x, y), (x + self._win_x, y + self._win_y), (0, 255, 0), 2)
+            cv2.imshow("Test Image Scanner", clone)
+            cv2.waitKey(1)
+            time.sleep(0.025)
+        
+    def _get_bb(self, y1, y2, x1, x2, scale_for_original):
+        """Get bounding box in the original input image"""
+        original_coords = [int(c / scale_for_original) for c in (y1, y2, x1, x2)]
+        return original_coords
 
 
 if __name__ == "__main__":
     import time
     
     image = cv2.imread("test_images//test1.jpg")[200:400, 200:400, :]
-    image_pyramid = ImgPyramid(image)
-    
-    for layer, _ in image_pyramid.generate_next():
-        image_scanner = ImgScanner(layer)
-        image_scanner.show_process()
+    scanner = ImgScanner(image)
+    scanner.show_process()
     
     
     
