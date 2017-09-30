400ed5ec5d9df8627a183a925a6da74ca3f49bad
jeongjoonsup <penny4860@gmail.com>
2017-09-30 15:01:54 +0900

    driver cleaning

--- 3_train.py
+++ 3_train.py
@@ -2,32 +2,7 @@
 
 from sklearn.svm import SVC
 from car.data import FileHDF5, create_xy
-
-
-CAR_DIR = "dataset//vehicles"
-NON_CAR_DIR = "dataset//non-vehicles"
-
-def evaluate_params(X, y, X_test, y_test, params):
-    from sklearn.model_selection import GridSearchCV
-    from sklearn.metrics import classification_report
-    clf = GridSearchCV(SVC(), params)
-    clf.fit(X, y)
-
-    print("==============================================================")
-    print("1. Best parameters set found on development set:")
-    print(clf.best_params_)
-    print("==============================================================")
-
-    print("2. Grid scores on development set:")
-    print()
-    means = clf.cv_results_['mean_test_score']
-    stds = clf.cv_results_['std_test_score']
-    for mean, std, params in zip(means, stds, clf.cv_results_['params']):
-        print("==============================================================")
-        print("    %0.3f (+/-%0.03f) for %r" % (mean, std * 2, params))
-        y_true, y_pred = y_test, clf.predict(X_test)
-        print(classification_report(y_true, y_pred))
-        print("==============================================================")
+from car.train import evaluate_params
 
 
 if __name__ == "__main__":
@@ -43,9 +18,12 @@ if __name__ == "__main__":
     tuned_parameters = [{'kernel': ['rbf'], 'gamma': [0.1, 1], 'C': [10]}]
 
     # {'kernel': 'rbf', 'gamma': 0.1, 'C': 10}
-    evaluate_params(X_train, y_train, X_test, y_test, tuned_parameters)
+    # evaluate_params(X_train, y_train, X_test, y_test, tuned_parameters)
 
-    clf = SVC()
-    clf.fit(X_train, y_train)    
+    clf = SVC(C=10.0, kernel='rbf', gamma=1.0)
+    clf.fit(X_train, y_train)
 
+    from sklearn.metrics import classification_report
+    y_true, y_pred = y_test, clf.predict(X_test)
+    print(classification_report(y_true, y_pred))
 
