60d87a34227c364f546e776d53859283e7f0e696
jeongjoonsup <penny4860@gmail.com>
2017-09-30 14:49:48 +0900

    nit

--- 3_train.py
+++ 3_train.py
@@ -13,18 +13,21 @@ def evaluate_params(X, y, X_test, y_test, params):
     clf = GridSearchCV(SVC(), params)
     clf.fit(X, y)
 
+    print("==============================================================")
     print("1. Best parameters set found on development set:")
-    print()
     print(clf.best_params_)
-    print()
+    print("==============================================================")
+
     print("2. Grid scores on development set:")
     print()
     means = clf.cv_results_['mean_test_score']
     stds = clf.cv_results_['std_test_score']
     for mean, std, params in zip(means, stds, clf.cv_results_['params']):
+        print("==============================================================")
         print("    %0.3f (+/-%0.03f) for %r" % (mean, std * 2, params))
         y_true, y_pred = y_test, clf.predict(X_test)
         print(classification_report(y_true, y_pred))
+        print("==============================================================")
 
 
 def create_xy(pos_features, neg_features):
