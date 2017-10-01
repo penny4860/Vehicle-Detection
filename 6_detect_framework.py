1cc826dc9f247b429207fa5d2a85b95cf90a85cd
jeongjoonsup <penny4860@gmail.com>
2017-10-01 20:36:39 +0900

    offset 반영한 전체 image plot 을 hard-coding

--- 6_detect_framework.py
+++ 6_detect_framework.py
@@ -7,13 +7,17 @@ from car.detect import ImgDetector
 
 if __name__ == "__main__":
     
-    img = cv2.imread("test_images//test1.jpg")[300:, :, :]
+    img = cv2.imread("test_images//test1.jpg")
     d = ImgDetector(classifier=load_model("model.pkl"))
-    img_draw = d.run(img)
+    img_draw = d.run(img[300:, :, :])
     print(len(d.detect_boxes))
     print(d.detect_boxes)
+    
+    import numpy as np   
+    img_ = np.concatenate([img[:300, :, :], img_draw], axis=0)
+    print(img_.shape)
 
-    cv2.imshow("Test Image Scanner", img_draw)
+    cv2.imshow("Test Image Scanner", img_)
     cv2.waitKey(0)
 
 
