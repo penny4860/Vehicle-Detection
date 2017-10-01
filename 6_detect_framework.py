cc3c99d74c95c321b2f9c614f60bc3ddda128399
jeongjoonsup <penny4860@gmail.com>
2017-10-01 20:26:12 +0900

    driver 정리

--- 6_detect_framework.py
+++ 6_detect_framework.py
@@ -1,28 +1,21 @@
 # -*- coding: utf-8 -*-
 
 import cv2
-from car.desc import get_hog_features
 from car.train import load_model
-import matplotlib.pyplot as plt
-import time
+from car.detect import ImgDetector
+
 
 if __name__ == "__main__":
-    from car.scan import Slider
-    clf = load_model("model.pkl")
     
     img = cv2.imread("test_images//test1.jpg")[300:, :, :]
-    slider = Slider(img)
+    d = ImgDetector(classifier=load_model("model.pkl"))
+    img_draw = d.run(img)
+    print(len(d.detect_boxes))
+    print(d.detect_boxes)
 
-    clone = img.copy()
-    for patch in slider.generate_next():
-        patch_gray = cv2.cvtColor(patch, cv2.COLOR_BGR2GRAY)
-        feature_vector = get_hog_features([patch_gray])
-        
-        # predict_proba
-        if clf.predict(feature_vector) == 1.0:
-            p1, p2 = slider.get_bb()
-            cv2.rectangle(clone, p1, p2, (0, 255, 0), 2)
-            cv2.imshow("Test Image Scanner", clone)
-            cv2.waitKey(1)
-            time.sleep(0.025)
+    cv2.imshow("Test Image Scanner", img_draw)
+    cv2.waitKey(0)
+
+
+
 
