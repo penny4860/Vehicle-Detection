8dbdc63809e0115675d0e6a59f42f23712c2ce85
jeongjoonsup <penny4860@gmail.com>
2017-10-02 14:48:18 +0900

    HeatMap.show_process() imp

--- car/heatmap.py
+++ car/heatmap.py
@@ -9,8 +9,10 @@ from scipy.ndimage.measurements import label
 class HeatMap(object):
     def __init__(self, threshold=3):
         self._threshold = threshold
-    
-    def get_boxes(self, boxes, w, h, plot=False):
+        self._heat_map = None
+        self._heat_bin = None
+        
+    def get_boxes(self, boxes, w, h):
         """
         # Args
             boxes : list of tuple (x1, y1, x2, y2)
@@ -19,43 +21,54 @@ class HeatMap(object):
             h : int
         
         # Returns
-            heat_map_boxes : list of tuple (x1, y1, x2, y2)
+            heat_boxes : list of tuple (x1, y1, x2, y2)
         """
 
-        heat_map = np.zeros((h, w)).astype(float)
+        self._heat_map = np.zeros((h, w)).astype(float)
     
         for box in boxes:
             x1, y1, x2, y2 = box
-            heat_map[y1:y2, x1:x2] += 1
+            self._heat_map[y1:y2, x1:x2] += 1
             
-        heat_map_bin = self._get_bin(heat_map)
-        heat_map_boxes = self._extract_boxes(heat_map_bin)
-        
-        if plot:
-            self._show_process(heat_map, heat_map_bin, heat_map_boxes)
-        
-        return heat_map_boxes
+        self._heat_bin = self._get_bin()
+        heat_boxes = self._extract_boxes()
+        return heat_boxes
 
-    def _show_process(self, heat_map, bin_map, boxes):
+    def show_process(self, image, boxes):
+        """
+        # Args
+            image : 
+                RGB-ordered original image
+            boxes : list of tuple
+                (x1, y1, x2, y2) ordered
+        """
         def _plot_images(images, titles):
             _, axes = plt.subplots(1, len(images), figsize=(10,10))
             for img, ax, text in zip(images, axes, titles):
                 ax.imshow(img, cmap="gray")
                 ax.set_title(text, fontsize=30)
             plt.show()
+            
+        heat_boxes = self.get_boxes(boxes, image.shape[1], image.shape[0])
 
-        drawn = np.zeros_like(heat_map)
+        drawn1 = np.zeros_like(image)
+        drawn2 = np.zeros_like(image)
+
         for box in boxes:
-            cv2.rectangle(drawn, (box[0], box[1]), (box[2], box[3]), (0,0,255), 6)
+            cv2.rectangle(drawn1, (box[0], box[1]), (box[2], box[3]), (0,0,255), 6)
+        
+        for box in heat_boxes:
+            cv2.rectangle(drawn2, (box[0], box[1]), (box[2], box[3]), (0,0,255), 6)
 
-        _plot_images([heat_map, bin_map, drawn], ["heat map", "thresholded heat map", "extracted boxes"])
+        _plot_images([self._heat_map, self._heat_bin, drawn1, drawn2],
+                     ["heat map", "thresholded heat map", "input boxes", "heat map processed boxes"])
 
-    def _get_bin(self, heat_map):
-        heat_map_bin = np.zeros_like(heat_map)
-        heat_map_bin[heat_map >= self._threshold] = 255
+    def _get_bin(self):
+        heat_map_bin = np.zeros_like(self._heat_map)
+        heat_map_bin[self._heat_map >= self._threshold] = 255
         return heat_map_bin
 
-    def _extract_boxes(self, heat_map_bin):
+    def _extract_boxes(self):
         """
         # Args
             heat_map : ndarray
@@ -75,8 +88,7 @@ class HeatMap(object):
             return (x1, y1, x2, y2)
             
         boxes = []
-        ccl_map, n_labels = label(heat_map_bin)
-        # Iterate through all detected carsccl_map[1]
+        ccl_map, n_labels = label(self._heat_bin)
         for car_number in range(1, n_labels+1):
             box = _box(ccl_map, car_number)
             boxes.append(box)
@@ -90,12 +102,10 @@ if __name__ == '__main__':
 
     img = plt.imread("..//test_images//test1.jpg")
     d = ImgDetector(classifier=load_model("..//model.pkl"))
-    img_draw = d.run(img, (0, 300))
+    d.run(img, (0, 300))
 
     heat_map = HeatMap()
-    boxes = heat_map.get_boxes(d.detect_boxes, img_draw.shape[1], img_draw.shape[0])
+    # boxes = heat_map.get_boxes(d.detect_boxes, img_draw.shape[1], img_draw.shape[0])
+    heat_map.show_process(img, d.detect_boxes)
     
-    for box in boxes:
-        cv2.rectangle(img, (box[0], box[1]), (box[2], box[3]), (0,0,255), 6)
-
-    plot_images([img, img_draw])
+    
