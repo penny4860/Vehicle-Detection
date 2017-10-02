604295b9a2dd92174fd71df2d3bab2fccf14ca13
jeongjoonsup <penny4860@gmail.com>
2017-10-02 14:18:24 +0900

    HeatMap class done

--- car/detect.py
+++ car/detect.py
@@ -70,81 +70,71 @@ class ImgDetector(object):
             cv2.rectangle(clone, p1, p2, (0, 255, 0), 2)
         return clone
 
-
+from scipy.ndimage.measurements import label
 class HeatMap(object):
     def __init__(self, threshold=3):
         self._threshold = threshold
     
-    def run(self, image, boxes):
-        heat_map = np.zeros((image.shape[0], image.shape[1])).astype(float)
+    def get_boxes(self, boxes, w, h):
+        heat_map = np.zeros((h, w)).astype(float)
     
         for box in boxes:
             x1, y1, x2, y2 = box
             heat_map[y1:y2, x1:x2] += 1
             
-        heat_map = self._get_bin(heat_map)
-        return heat_map
+        heat_map_bin = self._get_bin(heat_map)
+        heat_map_boxes = self._extract_boxes(heat_map_bin)
+        return heat_map_boxes
 
-    def _get_bin(self):
+    def _get_bin(self, heat_map):
         heat_map_bin = np.zeros_like(heat_map)
         heat_map_bin[heat_map >= self._threshold] = 255
         return heat_map_bin
 
-    def _set_boxes(self, heat_map):
+    def _extract_boxes(self, heat_map_bin):
         """
         # Args
             heat_map : ndarray
                 binary image
         """
-        pass
+        def _box(ccl_map, car_number):
+            # Find pixels with each car_number label value
+            nonzero = (ccl_map == car_number).nonzero()
+            # Identify x and y values of those pixels
+            nonzeroy = np.array(nonzero[0])
+            nonzerox = np.array(nonzero[1])
+            # Define a bounding box based on min/max x and y
+            x1 = np.min(nonzerox)
+            y1 = np.min(nonzeroy)
+            x2 = np.max(nonzerox)
+            y2 = np.max(nonzeroy)
+            return (x1, y1, x2, y2)
+            
+        boxes = []
+        ccl_map, n_labels = label(heat_map_bin)
+        # Iterate through all detected carsccl_map[1]
+        for car_number in range(1, n_labels+1):
+            box = _box(ccl_map, car_number)
+            boxes.append(box)
+        return boxes
 
         
 if __name__ == "__main__":
     from car.utils import plot_images
     import numpy as np
     
-#     img = cv2.imread("..//test_images//test1.jpg")
-#     d = ImgDetector(classifier=load_model("..//model.pkl"))
-#     img_draw = d.run(img, (0, 300))
-#     img_draw = cv2.cvtColor(img_draw, cv2.COLOR_BGR2RGB)
-#     heat_map = create_heat_map(img, d.detect_boxes)
-#     np.save("heatmap", heat_map)
+    img = cv2.imread("..//test_images//test1.jpg")
+    d = ImgDetector(classifier=load_model("..//model.pkl"))
+    img_draw = d.run(img, (0, 300))
 
-    heat_map = np.load("heatmap.npy")
-    heat_map_bin = np.zeros_like(heat_map)
-    heat_map_bin[heat_map >= 3] = 255
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    img_draw = cv2.cvtColor(img_draw, cv2.COLOR_BGR2RGB)
     
-    from scipy.ndimage.measurements import label
-    labels = label(heat_map_bin)
-    print(labels[1])
+    heat_map = HeatMap()
+    boxes = heat_map.get_boxes(d.detect_boxes, img_draw.shape[1], img_draw.shape[0])
     
-#     # plot_images([heat_map, heat_map_bin])
-#     import matplotlib.image as mpimg
-#     import matplotlib.pyplot as plt
-#     import numpy as np
-#     import cv2
-#     
-#     def draw_labeled_bboxes(img, labels):
-#         # Iterate through all detected cars
-#         for car_number in range(1, labels[1]+1):
-#             # Find pixels with each car_number label value
-#             nonzero = (labels[0] == car_number).nonzero()
-#             # Identify x and y values of those pixels
-#             nonzeroy = np.array(nonzero[0])
-#             nonzerox = np.array(nonzero[1])
-#             # Define a bounding box based on min/max x and y
-#             bbox = ((np.min(nonzerox), np.min(nonzeroy)), (np.max(nonzerox), np.max(nonzeroy)))
-#             # Draw the box on the image
-#             cv2.rectangle(img, bbox[0], bbox[1], (0,0,255), 6)
-#         # Return the image
-#         return img
-#     
-#     # Read in the last image above
-#     image = mpimg.imread('img105.jpg')
-#     # Draw bounding boxes on a copy of the image
-#     draw_img = draw_labeled_bboxes(np.copy(image), labels)
-#     # Display the image
-#     plt.imshow(draw_img)
+    for box in boxes:
+        cv2.rectangle(img, (box[0], box[1]), (box[2], box[3]), (0,0,255), 6)
 
-
-
+    plot_images([img, img_draw])
+     
