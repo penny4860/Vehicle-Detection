c990f8002f07315f7e0e9058b77979da5d4cc99e
jeongjoonsup <penny4860@gmail.com>
2017-10-05 14:55:47 +0900

    Revert "디버깅 setup"
    This reverts commit dab82ad0e686d3bd1557c5820fdef179ef63f6e3.

--- 6_detect_framework.py
+++ 6_detect_framework.py
@@ -4,21 +4,17 @@ import cv2
 from car.train import load_model
 from car.detect import ImgDetector
 
+
 if __name__ == "__main__":
     
-    img = cv2.imread("project_video//00642.jpg")
+    img = cv2.imread("project_video//00640.jpg")
+    img = cv2.imread("project_video//00700.jpg")
     d = ImgDetector(classifier=load_model("model_v4.pkl"))
-    img_draw = d.run(img, start_pt=(0,350), end_pt=(1280, 400+256), do_heat_map=True)
+    img_draw = d.run(img, do_heat_map=False)
     
     print(d.detect_boxes)
-    print(d.heat_boxes)
-    
-    import matplotlib.pyplot as plt
-    plt.imshow(img_draw)
-    plt.show()
-    
-#     cv2.imshow("Test Image Scanner", img_draw)
-#     cv2.waitKey(0)
+    cv2.imshow("Test Image Scanner", img_draw)
+    cv2.waitKey(0)
 
 
 
