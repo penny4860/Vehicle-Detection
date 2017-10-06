8fd63dc835ee0f8610a828725fb32dca8673b699
jeongjoonsup <penny4860@gmail.com>
2017-10-06 13:09:52 +0900

    overlapped object exp

--- 8_video.py
+++ 8_video.py
@@ -17,10 +17,10 @@ def files_to_images(files):
     images = np.array(images)
     return images
 
-START = 676
+START = 731
 if __name__ == "__main__":
     img_files = list_files("project_video", pattern="*.jpg", random_order=False, recursive_option=False)
-    imgs = files_to_images(img_files)[START:]
+    imgs = files_to_images(img_files)[START:START+100]
     
     d = VideoDetector(ImgDetector(classifier=load_model("model_v4.pkl")))
     # d = ImgDetector(classifier=load_model("model_v4.pkl"))
@@ -30,7 +30,7 @@ if __name__ == "__main__":
         img_draw = d.run(img)
         
         count_str = "{}".format(count).zfill(5)
-        filename = "project_video//video_track//{}.jpg".format(count_str)
+        filename = "project_video//debug//{}.jpg".format(count_str)
         cv2.imwrite(filename, img_draw)
         print(filename)
         count += 1
