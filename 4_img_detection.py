b3ea73a402760931931603ad5cab9594ab657641
jeongjoonsup <penny4860@gmail.com>
2017-10-07 16:10:38 +0900

    image detection test driver

---
+++ 4_img_detection.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from car.train import load_model
+from car.detect import ImgDetector, VideoDetector
+from car.data import list_files
+from car.utils import plot_images
+
+import cv2
+
+
+IMG_SRC_DIR = "..//project_video"
+
+
+if __name__ == "__main__":
+    img_files = list_files(IMG_SRC_DIR, pattern="*.jpg", random_order=False, recursive_option=False)    
+    d = ImgDetector(classifier=load_model("model_v4.pkl"))
+    
+    for fname in img_files:
+        img = cv2.imread(fname)
+        img_draw = d.run(img)
+        cv2.imshow("image detection framework", img_draw)
+        cv2.waitKey(0)
